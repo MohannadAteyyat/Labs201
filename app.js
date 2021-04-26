9fa87faae4e71d2b644b7f92ad090f57c749458e
MohannadAteyyat <ateyyatmohanad@gmail.com>
2021-04-26 17:59:03 +0300

    editing after graded

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 let userName=prompt('What\'s your name?')
-console.log('What\'s your name?'+ userName)
+console.log('What\'s your name?'+ ' '+ userName)
 
-let studyType=prompt('Hello' + userName +  'What engineering type you thinking of?')
+let studyType=prompt('Hello' + '  ' +  userName + '  ' +  'What engineering type you thinking of?')
 console.log('What engineering type you thinking of?' + studyType)
 
-let degree=prompt('That\'s amazing' + userName + 'what degree you intend to have?')
+let degree=prompt('That\'s amazing' + ' '+  userName + ' ' + + 'what degree you intend to have?')
 console.log('what degree you intend to have?' + degree)
 
-let time=prompt ('Allright' + userName + 'Final Question : When you will be ready to start your Engineering degree?')
+let time=prompt ('Allright' + ' '+  userName + ' ' + + 'Final Question : When you will be ready to start your Engineering degree?')
 
-console.log('When you will be ready to start your Engineering degree?' + time )
+console.log('When you will be ready to start your Engineering degree?' +' '+  time )
 
-alert('We are very sorry' + userName + 'The degree you chose is not available with this time try different time please' )
+alert('We are very sorry' + ' '+  userName + ' ' + + 'The degree you chose is not available with this time try different time please' )
 
